4ab2dea0a416ed7a5c9b4ca4e37ef0c66b565003
Jack <jack.walsh@my.jcu.edu.au>
2020-03-15 21:38:53 +1000

    add score function and random score generator

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,15 +2,29 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50 :
-    print("Passable")
-elif score < 50:
-    print("Bad")
+
+def main():
+    # score = float(input("Enter score: "))
+    # print(grade_score(score))
+
+    for i in range(1):
+        score = random.randint(1, 100)
+        print("Your score of {} is {}".format(score, grade_score(score)))
+
+
+def grade_score(score):
+    if score < 0:
+        return "Invalid score"
+    elif score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score < 50:
+        return "Bad"
+
+
+main()
