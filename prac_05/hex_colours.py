ce7d98f8c28b497b532da49c8577ed2b5cb4c576
Jack <jack.walsh@my.jcu.edu.au>
2020-04-05 19:38:20 +1000

    add hex_colours colour list for user

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,4 +1,8 @@
-HEX_COLOURS = {"AliceBlue": "#f0f8ff", "aquamarine1": "#7fffd4"}
+HEX_COLOURS = {"AliceBlue": "#f0f8ff", "aquamarine1": "#7fffd4", "azure1": "#f0ffff", "azure2": "#e0eeee",
+               "azure3": "#c1cdcd", "azure4": "#838b8b", "beige": "#f5f5dc", "bisque1": "#ffe4c4", "bisque2": "#eed5b7"}
+
+for colour in HEX_COLOURS:
+    print("{}".format(colour))
 
 colour = input("Enter name of colour: ")
 while colour != "":
