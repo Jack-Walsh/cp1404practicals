623eb21b867bdfb4a3116992406773c3b1f68cde
JackWalsh <jack.walsh@my.jcu.edu.au>
2020-04-30 23:08:00 +1000

    Add convert_miles_km .kv and .py

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.app import StringProperty
 
+MILES_TO_KM = 1.609344
+
 
 class MilesToKilometers(App):
     def build(self):
@@ -10,7 +12,21 @@ class MilesToKilometers(App):
         return self.root
 
     def calculate_kms(self):
-        self.root.ids.output_label.text = "Oops, m in the screenshot is not a good abbreviation for miles."
+        value = self.get_miles()
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
+
+    def increment_miles(self, change):
+        value = self.get_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.calculate_kms()
+
+    def get_miles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
 
 
 MilesToKilometers().run()
